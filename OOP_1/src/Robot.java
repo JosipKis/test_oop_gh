642964d2f19ec0f1c452bcdc54e84f06da14544f
JosipKis <josip.kis2@gmail.com>
2023-04-24 17:16:42 +0200

    Robot init implementation

--- OOP_1/src/Robot.java
+++ OOP_1/src/Robot.java
@@ -2,9 +2,46 @@ import java.util.Comparator;
 
 public class Robot implements Comparator<Robot> {
 
+    private String name;
+    private int id;
+    private static int cntID = 10;
+    private static final int MINL = 3;
+    private static final int MAXL = 15;
+    private static final String alph = "abcdefghijklmnoprstuvzxywABCDEFGHIJKLMNOPRSTUVZXYW0123456789";
+    public Robot(){
+        this.name = generateName();
+        this.id = cntID++;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    @Override
+    public int compare(Robot t1, Robot t2) {
+        if (t1.getName().length() > t2.getName().length()){
+            return 1;
+        }else if(t1.getName().length() < t2.getName().length()){
+            return -1;
+        }else{
+            return 0;
+        }
+    }
+
+    private String generateName(){
+        String name = "";
+        int lng_chars = (int) (Math.random() * (MAXL-MINL)) + MINL;
+        for (int k = 0; k < lng_chars; k++) {
+            int pos = (int) (Math.random() * (alph.length()) - 1);
+            name += alph.charAt(pos);
+        }
+        return name;
+    }
 
     @Override
-    public int compare(Robot robot, Robot t1) {
-        return 0;
+    public String toString() {
+        return "Robot{" +
+                "name='" + name + '\'' +
+                ", id=" + id +
+                '}';
     }
 }
---
+++ OOP_1/src/TestRobot.java
@@ -0,0 +1,12 @@
+public class TestRobot {
+    public static void main(String[] args) {
+//        String test = "skdjasdhadshGSDHJ";
+//        System.out.println(test);
+//        System.out.println(test.length());
+//        System.out.println(test.charAt(3));
+//        int pos =(int) (Math.random() * (test.length())-1);
+//        System.out.println("Position: "+ pos +" ch: "+ test.charAt(pos));
+        Robot robot1 = new Robot();
+        System.out.println(robot1);
+    }
+}
