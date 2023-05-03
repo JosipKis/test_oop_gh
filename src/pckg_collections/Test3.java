b9acee6a02738f446420719a017ea778964f3da9
JosipKis <josip.kis2@gmail.com>
2023-05-03 18:33:39 +0200

    Collections upgraded

--- src/pckg_collections/Test3.java
+++ src/pckg_collections/Test3.java
@@ -1,6 +1,8 @@
 package pckg_collections;
 
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Test3 {
     public static void main(String[] args) {
@@ -11,5 +13,27 @@ public class Test3 {
         mapa.put(90, "Divna");
         mapa.put(43, "Franka");
         System.out.println(mapa);
+        printMapElements(mapa);
+        Set<Integer> keys = getKeysForValue(mapa, "Franka");
+        System.out.println(keys);
+    }
+
+    private static void printMapElements(HashMap<Integer, String> mapa){
+        for (Integer key: mapa.keySet()){
+            System.out.println("Key: "+ key);
+            System.out.println("value: "+ mapa.get(key));
+        }
+    }
+
+    private static Set<Integer> getKeysForValue(HashMap<Integer, String> mapa, String value) {
+        Set<Integer> keys = new HashSet<>();
+        for (Integer key : mapa.keySet()) {
+            if (mapa.get(key).equals(value)) {
+                keys.add(key);
+            } else {
+                continue;
+            }
+        }
+        return keys;
     }
 }
